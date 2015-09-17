517037819b95030e79c0c333d1f73c99dd21867a
Gabriel Feitosa <gabfeitosa@gmail.com>
2015-09-17 16:31:42 -0300

    filter

--- angularjs/filter/app.js
+++ angularjs/filter/app.js
@@ -24,4 +24,11 @@
         return input;
       };
     });
+
+  angular.module('app-filters')
+    .controller('ExemploController', function($scope, $filter,currencyFilter) {
+      $scope.moedaFormatada = $filter('currency')(123456789);
+      $scope.moedaFormatada2 = currencyFilter(123456789);
+      $scope.dataFormatada = $filter('date')('1984-12-15T00:00','medium');
+    });
 })();
